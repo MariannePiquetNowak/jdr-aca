156f16e5aafb8b69cac47db8d5cfb5a3885731f2
Antho69-jdr <antho_b_69@msn.com>
2025-12-04 18:35:14 +0100

    Patch du bestiaire

--- src/routes/bestiaire.jsx
+++ src/routes/bestiaire.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import '../styles/components/_bestiaire.scss';
 import BestiaireForm from '../components/BestiaireForm';
